245da0d7be4e7c18a88b332ed33b6fe821b1aabb
Jemesson <jemessonlima@gmail.com>
2020-08-13 16:07:25 -0300

    Add NodeJS fundamentals challenge

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 > This project is the NodeJS challenge on GoStack bootcamp at the phase 2.
 
 ## Learned topics in this challenge:
+- [DTO](https://medium.com/@Colin_But/dissecting-the-dto-pattern-ac3e54d0e4c8)
 - [SOLID](https://www.notion.so/Princ-pios-do-SOLID-d469618bbd2049668eaf80e889194cce)
    - Models
    - Repositories
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceFn = (accumulator: Balance, current: Transaction): Balance => {
+      switch (current.type) {
+        case 'income':
+          accumulator.income += current.value;
+          break;
+        case 'outcome':
+          accumulator.outcome += current.value;
+          break;
+        default:
+          break;
+      }
+
+      return accumulator;
+    };
+
+    const { income, outcome } = this.transactions.reduce(balanceFn, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
